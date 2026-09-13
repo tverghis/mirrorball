553c96f67c2f5fed1f51010a39d78eb428d04119
Tarun Verghis <tarun.verghis@gmail.com>
2026-09-12 20:17:23 -0700

    Define UploadChunksResponse and headers for chunk upload

--- mirrorball-api/src/lib.rs
+++ mirrorball-api/src/lib.rs
@@ -2,6 +2,11 @@ use std::num::{NonZeroU64, NonZeroUsize};
 
 use serde::{Deserialize, Serialize};
 
+pub mod chunks;
+
+pub const UPLOAD_TOKEN_HEADER: &str = "x-mirb-token";
+pub const CHUNK_HASHES_HEADER: &str = "x-mirb-chunk-hashes";
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CreateUploadRequest {
     pub file_size: NonZeroU64,
@@ -12,6 +17,11 @@ pub struct CreateUploadRequest {
 #[derive(Debug, Deserialize, Serialize)]
 pub struct CreateUploadResponse {
     pub token: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct UploadChunksResponse {
+    pub chunk_count: usize,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
